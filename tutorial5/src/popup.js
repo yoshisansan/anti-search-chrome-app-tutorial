52a769e59cd35cc7add20a2cd11f037cb07d3576
AKIFUMI <awakewalker007@example.com>
2021-11-17 17:25:24 +0700

    エラー修正

--- tutorial5/src/popup.js
+++ tutorial5/src/popup.js
@@ -11,11 +11,12 @@ const getSaveData = () => new Promise(resolve => {
 
 addDomainBtn.addEventListener('click', async(e) => {
   e.preventDefault();
-  const inputElm = document.getElementById('InputStr');
-  const domainRegex = /(?:https?:\/\/www.|https?:\/\/)([^\/]+)/;
-  const domain = inputElm.value.match(domainRegex)[1];
-  if(domain === null) return;
+  const inputElm = document.getElementById('InputStr'),
+    domainRegex = /(?:https?:\/\/www.|https?:\/\/)([^\/]+)/,
+    hasDomainURL = inputElm.value.match(domainRegex);
+  if(hasDomainURL === null) return;
 
+  const domain = hasDomainURL[1];
   const saveDataObj = await getSaveData().then((res) => res);
   const length = Object.keys(saveDataObj).length;
   chrome.storage.sync.set({[`key${length + 1}`]: domain}, () => {
@@ -28,7 +29,6 @@ addDomainBtn.addEventListener('click', async(e) => {
 // Read処理 //
 const readSaveData = () => {
   chrome.storage.sync.get(null, (res) => {
-    console.log((res));
     const domainDOMs = Object.values(res).map((domain, i) => `<li class="domains__list">${domain}<button id="key${i + 1}" class="domains__list-btn">消す</button></li>`).join('');
     domainLists.innerHTML = domainDOMs;
   });
@@ -49,10 +49,10 @@ const observer = new MutationObserver(async(records) => {
   const domainLists = Array.from(records[0].addedNodes);
   domainLists.forEach( async(li) => {
     li.querySelector('.domains__list-btn').addEventListener('click', async(e) => {
-      // 消すボタンを押すと対象エレメントを消してkey1,key2...で入れ直す処理
-      await chrome.storage.sync.remove(e.target.id);
+      // 消すボタンを押すと対象プロパティを消してkey1,key2...で入れ直す処理
+      chrome.storage.sync.remove(e.target.id);
       const saveDataObj = await getSaveData().then((res) => res);
-      await chrome.storage.sync.clear();
+      chrome.storage.sync.clear();
       Object.values(saveDataObj).map((domain, i) => chrome.storage.sync.set({[`key${i + 1}`]: domain}));
       readSaveData();
     });
