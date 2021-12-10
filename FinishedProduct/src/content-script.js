b07edd7ec75b8b0e28da3b18ad210a25b359e6ef
AKIFUMI <awakewalker007@example.com>
2021-12-10 17:51:15 +0700

    プルリク用のディレクトリ

---
+++ FinishedProduct/src/content-script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// tutorial2
+// 任意のURLを検索結果に表示しないようにする
+const getSaveData = () =>
+  new Promise((resolve) => {
+    chrome.storage.sync.get(null, resolve);
+  });
+
+const googleTargetDOM = Array.from(document.querySelectorAll('#rso > div'));
+
+const removeTargetDom = (targetDeleteDomains, targetDOM) => {
+  // 取得したNodeListはArray.form()で配列化して配列として扱えるようにする
+  targetDOM.map((dom) => {
+    const DOMofIncludingUrl = dom.querySelector('div > cite');
+    if (DOMofIncludingUrl === null || DOMofIncludingUrl === undefined) return;
+    // innerTextの中身 'https://developer.mozilla.org › API' のようになっており小さい空白部分をsplitさせる
+    const searchedUrlText = DOMofIncludingUrl.innerText.split(' ')[0],
+      domainRegex = /(?:https?:\/\/www.|https?:\/\/)([^\/]+)/,
+      searchedUrlDomain = domainRegex.test(searchedUrlText)
+        ? searchedUrlText.match(domainRegex)[1]
+        : '';
+    // findIndexはマッチしない場合の返り値が -1 となる
+    const hasDomain = targetDeleteDomains.findIndex(
+      (domain) => searchedUrlDomain === domain,
+    );
+    if (hasDomain > -1) dom.parentElement.removeChild(dom);
+  });
+};
+
+(async () => {
+  const saveData = await getSaveData().then((res) => res);
+  const targetDomains = Object.values(saveData);
+  removeTargetDom(targetDomains, googleTargetDOM);
+})();
